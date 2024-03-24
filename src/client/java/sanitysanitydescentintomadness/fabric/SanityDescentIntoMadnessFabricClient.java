327bb5fd8ea860cafa75e13727a7cc27525110d6
1 <1@1>
2024-03-24 16:15:25 +0800

    .

---
+++ src/client/java/sanitysanitydescentintomadness/fabric/SanityDescentIntoMadnessFabricClient.java
@@ -0,0 +1,10 @@
+package sanitysanitydescentintomadness.fabric;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SanityDescentIntoMadnessFabricClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/main/java/sanitysanitydescentintomadness/fabric/SanityDescentIntoMadnessFabricDataGenerator.java
@@ -0,0 +1,11 @@
+package sanitysanitydescentintomadness.fabric;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class SanityDescentIntoMadnessFabricDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
